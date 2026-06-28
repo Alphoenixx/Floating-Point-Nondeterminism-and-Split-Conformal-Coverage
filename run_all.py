c4eff04edab0c19bbd18338cd12abed70ac55ca2
Alphoenixx <87995058+Alphoenixx@users.noreply.github.com>
2026-06-28 21:14:38 +0530

    Delete run_all.py

--- run_all.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import subprocess
-
-HERE = os.path.dirname(os.path.abspath(__file__))
-
-for script in ["synthetic_experiment.py", "real_experiment.py"]:
-    subprocess.run([sys.executable, os.path.join(HERE, script)], check=True)
